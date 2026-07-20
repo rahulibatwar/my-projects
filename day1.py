22d6a3d74336ebd050a2e3244b2a07306f39de91
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-07-20 10:19:41 +0530

    2th code self

---
+++ day1.py
@@ -0,0 +1,10 @@
+# 1. ask the user to enter their name 
+user_name = input("Please enter your name: ")
+
+# 2. ask the user for their favorite suparhero
+favorite_superhero = input("Please enter your favorite superhero: ")
+
+# 3. print a greeting massage 
+print("Hello " + user_name + "!")
+print("Your favorite superhero is " + favorite_superhero + "!")
+
