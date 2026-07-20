48c1c01680281b9ba01939bef6955eaeb53cb1e7
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-07-20 09:50:35 +0530

    1th code self

---
+++ collage.py
@@ -0,0 +1,14 @@
+# step 1: storing data in variable
+hero_name = "Iron Man"
+iron_suits = 85 
+flying_speed = 2.5 #mach speed 
+
+# step 2: printing the variables
+print(hero_name)
+print(iron_suits)
+
+#step 3: combining  text and variables together 
+print("our hero is " + hero_name)
+print("He has", iron_suits, "suits!")
+print("He has", iron_suits, "suits!")
+
