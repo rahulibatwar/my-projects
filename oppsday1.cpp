4425f6f57fb050b5b5a1dc3d26a119e15528a05e
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-08-16 17:29:36 +0530

    non-parameterized constructor & parameterized constructor

--- oppsday1.cpp
+++ oppsday1.cpp
@@ -3,42 +3,42 @@
 using namespace std;
 
 class teacher {
-    public:
+public:
+     // non-parameterized constructor
+      teacher(){
+        dept = "Computer Science";
+      }
+
+      // parameterized constructor
+      teacher(string n, string s, string d, string e, double sal) {
+        name = n;
+        subject = s;
+        dept = d;
+        email = e;
+        salary = sal;
+      } 
+
     // properties // attributes
       string name;
       string subject;
       string dept;
       string email;
-    private: 
       double salary;
-
-    public:
-
       // methods // functions
       void changedept(string newdept) {
         dept = newdept;
-    }
-
-      //setter 
-      void setsalary(double s) {
-        salary = s;
-    }
-
-       //getter
-      double getsalary() {
-        return salary;
-    }
-};
+      }
+      void getinfo() {
+        cout << "Name: " << name << endl;
+        cout << "Subject: " << subject << endl;
+        cout << "Department: " << dept << endl;
+        cout << "Email: " << email << endl;
+        cout << "Salary: $" << salary << endl;
+      }
+    };
 
 int main() {
-    teacher t1;
-    t1.name = "John Doe";
-    t1.subject = "Mathematics";
-    t1.dept = "Computer Science";
-    t1.setsalary(75000);
-
-    cout << t1.name << endl;
-    cout << t1.getsalary() << endl;
-
+    teacher t1("John Doe", "Data Structures", "Computer Science", "john.doe@university.edu", 75000);
+    t1.getinfo();
     return 0;
 }
