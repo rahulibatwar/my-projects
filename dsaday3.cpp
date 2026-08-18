bcfef4554b5cd25773815fb6ff54efbde3636c78
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-08-18 12:07:09 +0530

    bitwise opretor

--- dsaday3.cpp
+++ dsaday3.cpp
@@ -2,24 +2,12 @@
 using namespace std;
 
 int main() { 
+    // int a = 3, b = 7;
+    cout << (6 & 10) << endl; // Bitwise AND
+    cout << (6 | 10) << endl; // Bitwise OR
+    cout << (6 ^ 10) << endl; // Bitwise XOR
+    cout << (10 << 2) << endl; // Left Shift
+    cout << (10 >> 1) << endl; // Right Shift
 
-
-       int a[7] = {2,5,6,12,54,3,7}; //
-
-       int mx = a[0]; //initialization
-
-       int count = 0;
-
-       for (int i=0; i<7; i++)
-       {
-        //check if the current element a[i] is even
-        if(a[i]%2 == 0)
-        {
-            count = count+1;
-        }
-       
-}
-
-cout << "the number of even elements = " << count << endl;
-
+    return 0;
 }
