957fd56913dc6fa9a899a8a705b907abe18502cd
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-08-18 19:26:54 +0530

    unsigned int valu ben to decimel conversing

--- day3dsa.cpp
+++ day3dsa.cpp
@@ -2,20 +2,9 @@
 using namespace std;    
 
 int main() {
-    // int a = 3 , b = 20;
-    // cout <<(6 & 10) << endl; // bitwise AND
-    // cout <<(6 | 10) << endl; // bitwise OR
-    // cout <<(6 ^ 10) << endl; // bitwise XOR
-    // cout <<(10 << 2) << endl; // left shift
-    // cout <<(10 >> 2) << endl; // right shift
-    // cout <<(5 - 2*6) << endl; // operator precedence
-    // cout <<((5 - 2)*6) << endl; // operator precedence
-    // cout <<(4 * 5 % 2) << endl; // operator precedence
-    cout << sizeof( int ) << endl; // size of int
-    cout << sizeof( long long int ) << endl; // size of long int
-    cout << sizeof( short int ) << endl; // size of short int
-    cout << sizeof( signed int ) << endl; // size of signed int
-    cout << sizeof( unsigned int ) << endl; // size of unsigned int
+    unsigned int x = -10;
+
+    cout << x << endl;  //bin to decimal conversion
 
     return 0;
 
