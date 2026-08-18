811a6bca8c01d2c539911f03f19289c73f2a4c09
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-08-18 19:34:23 +0530

    Check if a Number is Power of 2 (Without Any Loop) The Core Bitwise Concept

--- day3dsa.cpp
+++ day3dsa.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
-using namespace std;    
+using namespace std;
+
+bool isPowerOfTwo(int n) {
+    // Condition 1: n must be greater than 0
+    // Condition 2: (n & (n - 1)) must evaluate to 0
+    return (n > 0) && ((n & (n - 1)) == 0);
+}
 
 int main() {
-    unsigned int x = -10;
-
-    cout << x << endl;  //bin to decimal conversion
-
+    int num = 32;
+    if (isPowerOfTwo(num)) {
+        cout << num << " is a power of 2." << endl;
+    } else {
+        cout << num << " is NOT a power of 2." << endl;
+    }
     return 0;
-
-
-
 }
