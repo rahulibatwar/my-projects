c1aa6cbc8e8abb362b6c466ced97cce9a84f070b
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-08-21 20:44:06 +0530

    vector syntax using cpp

--- day6dsa.cpp
+++ day6dsa.cpp
@@ -1,31 +1,9 @@
 #include <iostream>
-#include <algorithm> //swap()
-
+#include <vector>
 using namespace std;
 
-
-void reverseArray(int arr[], int sz){
-    int start = 0, end = sz-1;
-
-
-    while(start < end){
-        swap(arr[start],arr[end]);
-        start++;
-        end--;
-
-    }
-}
-
 int main(){
-    int arr [] = {4, 2, 7, 8, 1};
-    int sz =5;
-
-    reverseArray(arr, sz);
-
-    for(int i=0; i<sz; i++){
-    cout << arr [i] << " ";
-    }
-    cout << endl;
-    return 0;
+  vector<int> vec = {1, 2, 3}; //3
+  cout << vec[0] << endl; 
 
 }
