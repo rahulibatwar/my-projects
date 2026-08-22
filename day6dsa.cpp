3db705d88c1318f9538f384dc6d4c4c3d3f9733e
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-08-22 18:42:49 +0530

    day6dsa.cpp array in char valuve

--- day6dsa.cpp
+++ day6dsa.cpp
@@ -3,12 +3,10 @@
 using namespace std;
 
 int main()  {
-  vector<int> vec(5, 0); 
-  cout << vec[0] << endl; 
-  cout << vec[1] << endl;
-  cout << vec[2] << endl;
-  cout << vec[3] << endl;
-  cout << vec[4] << endl;
-
+  vector<char> vec = {'a', 'b', 'c', 'd', 'e'}; 
+  
+  for(char i : vec) {
+    cout << i << endl;
+  }
      return 0;
 }
