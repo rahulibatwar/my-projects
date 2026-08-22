c42beeca8a8f7f20783d27c83e71390f9c0bef9f
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-08-22 18:38:23 +0530

    day6dsa.cpp array in vector

--- day6dsa.cpp
+++ day6dsa.cpp
@@ -2,8 +2,13 @@
 #include <vector>
 using namespace std;
 
-int main(){
-  vector<int> vec = {1, 2, 3}; //3
+int main()  {
+  vector<int> vec(5, 0); 
   cout << vec[0] << endl; 
+  cout << vec[1] << endl;
+  cout << vec[2] << endl;
+  cout << vec[3] << endl;
+  cout << vec[4] << endl;
 
+     return 0;
 }
