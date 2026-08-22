1fd03596528268726664e31cafb4ac9e3d76c788
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-08-22 18:57:18 +0530

    day6dsa.cpp array in vec.front& vec.back

--- day6dsa.cpp
+++ day6dsa.cpp
@@ -3,13 +3,18 @@
 using namespace std;
 
 int main()  {
-  vector<char> vec = {'a', 'b', 'c', 'd', 'e', 'f'}; 
-
-  cout << "size = " << vec.size() << endl;
+  vector<int> vec; 
   vec.push_back(34);
+  vec.push_back(35);
+  vec.push_back(36);
   cout << "after push back size = " << vec.size() << endl;
-  for(char i : vec) {
-    cout << i << endl;
-  }
+
+  vec.pop_back();
+  // for(int i : vec) {
+  //   cout << i << endl;
+
+  cout << vec.front() << endl; 
+  cout << vec.back() << endl;
+  
      return 0;
 }
