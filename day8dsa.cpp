7414cac66570c507703f2030185cfc2ae38376a4
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-08-23 23:09:39 +0530

    vectors in array using a dsa max sabereas sum

--- day8dsa.cpp
+++ day8dsa.cpp
@@ -6,13 +6,19 @@ int main () {
     int n = 5;
     int arr[5] = {1, 2, 3, 4, 5};
 
+    int maxsum = INT_MIN;
+
     for(int st=0; st<n; st++) {
+        int currsum = 0;
         for(int end=st; end<n; end++) {
-            for(int i=st; i<=end; i++) {
-                cout << arr[i];
-            }   
-            cout << " ";
-        }
-        cout << endl;
+            currsum += arr[end];
+            maxsum = max(currsum, maxsum);  
+           
     }
+}
+
+cout << "max subarray sum = " << maxsum << endl;
+
+return 0;
+
 }
