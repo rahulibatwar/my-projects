550dea82a7d8898db48e6c39f1488859e69fe51b
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-08-27 22:41:18 +0530

    Add solution for LeetCode 5: Longest Palindromic Substring

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,98 +1,46 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
-    string generateString(string str1, string str2) {
-        int n = str1.length();
-        int m = str2.length();
-        int len = n + m - 1;
+    string longestPalindrome(string s) {
+        if (s.empty()) return "";
+        int start = 0, maxLen = 0;
 
-        string ans(len, '?');
-        vector<bool> fixed(len, false);
+        auto expandAroundCenter = [&](int left, int right) {
+            while (left >= 0 && right < s.length() && s[left] == s[right]) {
+                left--;
+                right++;
+            }
+            return right - left - 1;
+        };
 
-        for (int i = 0; i < n; i++) {
-            if (str1[i] == 'T') {
-                for (int j = 0; j < m; j++) {
-                    if (ans[i + j] != '?' && ans[i + j] != str2[j]) {
-                        return "";
-                    }
-                    ans[i + j] = str2[j];
-                    fixed[i + j] = true;
-                }
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(i, i);
+            int len2 = expandAroundCenter(i, i + 1);
+            int len = max(len1, len2);
+
+            if (len > maxLen) {
+                maxLen = len;
+                start = i - (len - 1) / 2;
             }
         }
 
-        for (int i = 0; i < len; i++) {
-            if (ans[i] == '?') {
-                ans[i] = 'a';
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (str1[i] == 'F') {
-                bool match = true;
-                for (int j = 0; j < m; j++) {
-                    if (ans[i + j] != str2[j]) {
-                        match = false;
-                        break;
-                    }
-                }
-
-                if (match) {
-                    bool changed = false;
-                    for (int j = m - 1; j >= 0; j--) {
-                        if (!fixed[i + j]) {
-                            char original = ans[i + j];
-                            for (char c = 'a'; c <= 'z'; c++) {
-                                if (c != str2[j]) {
-                                    ans[i + j] = c;
-                                    break;
-                                }
-                            }
-                            
-                            bool valid = true;
-                            for (int k = 0; k <= i; k++) {
-                                if (str1[k] == 'F') {
-                                    bool m_check = true;
-                                    for (int l = 0; l < m; l++) {
-                                        if (ans[k + l] != str2[l]) {
-                                            m_check = false;
-                                            break;
-                                        }
-                                    }
-                                    if (m_check) {
-                                        valid = false;
-                                        break;
-                                    }
-                                }
-                            }
-
-                            if (valid) {
-                                changed = true;
-                                break;
-                            } else {
-                                ans[i + j] = original;
-                            }
-                        }
-                    }
-                    if (!changed) return "";
-                }
-            }
-        }
-
-        return ans;
+        return s.substr(start, maxLen);
     }
 };
 
 int main() {
     Solution sol;
 
-    string str1 = "TFTF", str2 = "ab";
-    cout << "Test Case 1 Output: " << sol.generateString(str1, str2) << " (Expected: ababa)" << endl;
+    string s1 = "babad";
+    cout << "Test Case 1 Output: " << sol.longestPalindrome(s1) << " (Expected: bab or aba)" << endl;
+
+    string s2 = "cbbd";
+    cout << "Test Case 2 Output: " << sol.longestPalindrome(s2) << " (Expected: bb)" << endl;
 
     return 0;
 }
