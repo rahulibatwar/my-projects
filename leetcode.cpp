fcd938d590a4cee453cbf2d5f27ee7002c890ad2
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-08-30 21:03:23 +0530

    Add solution for LeetCode 10: Regular Expression Matching

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,35 +1,52 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
 class Solution {
 public:
-    bool isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
-            return false;
+    bool isMatch(string s, string p) {
+        int m = s.length();
+        int n = p.length();
+
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        dp[0][0] = true;
+
+        for (int j = 2; j <= n; j++) {
+            if (p[j - 1] == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
         }
 
-        int reversedHalf = 0;
-        while (x > reversedHalf) {
-            reversedHalf = reversedHalf * 10 + x % 10;
-            x /= 10;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p[j - 1] == s[i - 1] || p[j - 1] == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    if (p[j - 2] == s[i - 1] || p[j - 2] == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
         }
 
-        return x == reversedHalf || x == reversedHalf / 10;
+        return dp[m][n];
     }
 };
 
 int main() {
     Solution sol;
 
-    int x1 = 121;
-    cout << "Test Case 1 Output: " << (sol.isPalindrome(x1) ? "true" : "false") << " (Expected: true)" << endl;
+    string s1 = "aa", p1 = "a";
+    cout << "Test Case 1 Output: " << (sol.isMatch(s1, p1) ? "true" : "false") << " (Expected: false)" << endl;
 
-    int x2 = -121;
-    cout << "Test Case 2 Output: " << (sol.isPalindrome(x2) ? "true" : "false") << " (Expected: false)" << endl;
+    string s2 = "aa", p2 = "a*";
+    cout << "Test Case 2 Output: " << (sol.isMatch(s2, p2) ? "true" : "false") << " (Expected: true)" << endl;
 
-    int x3 = 10;
-    cout << "Test Case 3 Output: " << (sol.isPalindrome(x3) ? "true" : "false") << " (Expected: false)" << endl;
+    string s3 = "ab", p3 = ".*";
+    cout << "Test Case 3 Output: " << (sol.isMatch(s3, p3) ? "true" : "false") << " (Expected: true)" << endl;
 
     return 0;
 }
