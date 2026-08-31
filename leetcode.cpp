51a0a2b8c7644fd716c2056b39987b59b176cdf4
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-08-31 23:24:17 +0530

    Add solution for LeetCode 13: Roman to Integer

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,15 +1,43 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        num_map = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in num_map:
-                return [num_map[complement], i]
-            num_map[num] = i
-        return []
+#include <iostream>
+#include <unordered_map>
+#include <string>
 
-if __name__ == "__main__":
-    sol = Solution()
-    print("Test Case 1 Output:", sol.twoSum([2, 7, 11, 15], 9)) # Expected: [0, 1]
-    print("Test Case 2 Output:", sol.twoSum([3, 2, 4], 6))      # Expected: [1, 2]
-    print("Test Case 3 Output:", sol.twoSum([3, 3], 6))         # Expected: [0, 1]
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> m = {
+            {'I', 1}, {'V', 5}, {'X', 10},
+            {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+
+        int total = 0;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            if (i < n - 1 && m[s[i]] < m[s[i + 1]]) {
+                total -= m[s[i]];
+            } else {
+                total += m[s[i]];
+            }
+        }
+
+        return total;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    string s1 = "III";
+    cout << "Test Case 1 Output: " << sol.romanToInt(s1) << " (Expected: 3)" << endl;
+
+    string s2 = "LVIII";
+    cout << "Test Case 2 Output: " << sol.romanToInt(s2) << " (Expected: 58)" << endl;
+
+    string s3 = "MCMXCIV";
+    cout << "Test Case 3 Output: " << sol.romanToInt(s3) << " (Expected: 1994)" << endl;
+
+    return 0;
+}
