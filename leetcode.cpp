b5c09db5d3d1c6fb2996afc12996064749d842d2
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-08-31 23:23:22 +0530

    Add solution for LeetCode 1: Two Sum in Python

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,27 +1,15 @@
-#include <vector>
-#include <algorithm>
+class Solution(object):
+    def twoSum(self, nums, target):
+        num_map = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+        return []
 
-class Solution {
-public:
-    int maxArea(std::vector<int>& height) {
-        int left = 0;
-        int right = height.size() - 1;
-        int max_water = 0;
-
-        while (left < right) {
-            int width = right - left;
-            int current_height = std::min(height[left], height[right]);
-            int current_water = width * current_height;
-
-            max_water = std::max(max_water, current_water);
-
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-
-        return max_water;
-    }
-};
+if __name__ == "__main__":
+    sol = Solution()
+    print("Test Case 1 Output:", sol.twoSum([2, 7, 11, 15], 9)) # Expected: [0, 1]
+    print("Test Case 2 Output:", sol.twoSum([3, 2, 4], 6))      # Expected: [1, 2]
+    print("Test Case 3 Output:", sol.twoSum([3, 3], 6))         # Expected: [0, 1]
