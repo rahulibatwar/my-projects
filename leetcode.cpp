d8baf314a97a40b3e55eb1c0c7d9a88225a09871
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-09-01 22:13:50 +0530

    Add solution for LeetCode 2: Add Two Numbers

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,43 +1,68 @@
 #include <iostream>
-#include <unordered_map>
-#include <string>
 
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
-    int romanToInt(string s) {
-        unordered_map<char, int> m = {
-            {'I', 1}, {'V', 5}, {'X', 10},
-            {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
-        };
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* curr = dummyHead;
+        int carry = 0;
 
-        int total = 0;
-        int n = s.length();
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int x = (l1 != nullptr) ? l1->val : 0;
+            int y = (l2 != nullptr) ? l2->val : 0;
+            int sum = carry + x + y;
 
-        for (int i = 0; i < n; i++) {
-            if (i < n - 1 && m[s[i]] < m[s[i + 1]]) {
-                total -= m[s[i]];
-            } else {
-                total += m[s[i]];
-            }
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+
+            if (l1 != nullptr) l1 = l1->next;
+            if (l2 != nullptr) l2 = l2->next;
         }
 
-        return total;
+        ListNode* result = dummyHead->next;
+        delete dummyHead;
+        return result;
     }
 };
+
+ListNode* createList(initializer_list<int> vals) {
+    ListNode* dummy = new ListNode(0);
+    ListNode* curr = dummy;
+    for (int val : vals) {
+        curr->next = new ListNode(val);
+        curr = curr->next;
+    }
+    ListNode* head = dummy->next;
+    delete dummy;
+    return head;
+}
+
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << (head->next != nullptr ? " -> " : "");
+        head = head->next;
+    }
+    cout << endl;
+}
 
 int main() {
     Solution sol;
 
-    string s1 = "III";
-    cout << "Test Case 1 Output: " << sol.romanToInt(s1) << " (Expected: 3)" << endl;
-
-    string s2 = "LVIII";
-    cout << "Test Case 2 Output: " << sol.romanToInt(s2) << " (Expected: 58)" << endl;
-
-    string s3 = "MCMXCIV";
-    cout << "Test Case 3 Output: " << sol.romanToInt(s3) << " (Expected: 1994)" << endl;
+    ListNode* l1 = createList({2, 4, 3});
+    ListNode* l2 = createList({5, 6, 4});
+    cout << "Test Case 1 Output: ";
+    printList(sol.addTwoNumbers(l1, l2)); // Expected: 7 -> 0 -> 8
 
     return 0;
 }
