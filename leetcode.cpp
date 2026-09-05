e1f4093ef22395a9fc13906c5bd01b53c3654754
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-09-05 22:45:20 +0530

    Add solution for LeetCode 26: Remove Duplicates from Sorted Array

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,70 +1,42 @@
 #include <iostream>
-#include <initializer_list>
+#include <vector>
 
 using namespace std;
 
-// Definition for singly-linked list node
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode dummy(0);
-        ListNode* tail = &dummy;
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
 
-        while (list1 != nullptr && list2 != nullptr) {
-            if (list1->val <= list2->val) {
-                tail->next = list1;
-                list1 = list1->next;
-            } else {
-                tail->next = list2;
-                list2 = list2->next;
+        int insertIndex = 1;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[insertIndex] = nums[i];
+                insertIndex++;
             }
-            tail = tail->next;
         }
 
-        tail->next = (list1 != nullptr) ? list1 : list2;
-
-        return dummy.next;
+        return insertIndex;
     }
 };
-
-// Helper function to create linked list from initializer list
-ListNode* createList(initializer_list<int> vals) {
-    ListNode dummy(0);
-    ListNode* curr = &dummy;
-    for (int val : vals) {
-        curr->next = new ListNode(val);
-        curr = curr->next;
-    }
-    return dummy.next;
-}
-
-// Helper function to print linked list
-void printList(ListNode* head) {
-    while (head != nullptr) {
-        cout << head->val << (head->next != nullptr ? " -> " : "");
-        head = head->next;
-    }
-    cout << endl;
-}
 
 int main() {
     Solution sol;
 
-    // Test Case 1: list1 = [1, 2, 4], list2 = [1, 3, 4]
-    ListNode* l1 = createList({1, 2, 4});
-    ListNode* l2 = createList({1, 3, 4});
+    // Test Case 1: [1, 1, 2] -> k = 2, nums = [1, 2]
+    vector<int> nums1 = {1, 1, 2};
+    int k1 = sol.removeDuplicates(nums1);
+    cout << "Test Case 1 (k = " << k1 << "): ";
+    for (int i = 0; i < k1; i++) cout << nums1[i] << " ";
+    cout << endl;
 
-    cout << "Merged Output: ";
-    ListNode* mergedHead = sol.mergeTwoLists(l1, l2);
-    printList(mergedHead); // Expected: 1 -> 1 -> 2 -> 3 -> 4 -> 4
+    // Test Case 2: [0,0,1,1,1,2,2,3,3,4] -> k = 5, nums = [0, 1, 2, 3, 4]
+    vector<int> nums2 = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    int k2 = sol.removeDuplicates(nums2);
+    cout << "Test Case 2 (k = " << k2 << "): ";
+    for (int i = 0; i < k2; i++) cout << nums2[i] << " ";
+    cout << endl;
 
     return 0;
 }
