6d70592c5deb816e70088e333b1a03192ed17268
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-09-06 11:49:21 +0530

    Add solution for LeetCode 27: Remove Element

--- leetcode.cpp
+++ leetcode.cpp
@@ -5,35 +5,33 @@ using namespace std;
 
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        if (nums.empty()) return 0;
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
 
-        int insertIndex = 1;
-
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] != nums[i - 1]) {
-                nums[insertIndex] = nums[i];
-                insertIndex++;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
             }
         }
 
-        return insertIndex;
+        return k;
     }
 };
 
 int main() {
     Solution sol;
 
-    // Test Case 1: [1, 1, 2] -> k = 2, nums = [1, 2]
-    vector<int> nums1 = {1, 1, 2};
-    int k1 = sol.removeDuplicates(nums1);
+    // Test Case 1: nums = [3,2,2,3], val = 3 -> k = 2, nums = [2,2]
+    vector<int> nums1 = {3, 2, 2, 3};
+    int k1 = sol.removeElement(nums1, 3);
     cout << "Test Case 1 (k = " << k1 << "): ";
     for (int i = 0; i < k1; i++) cout << nums1[i] << " ";
     cout << endl;
 
-    // Test Case 2: [0,0,1,1,1,2,2,3,3,4] -> k = 5, nums = [0, 1, 2, 3, 4]
-    vector<int> nums2 = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-    int k2 = sol.removeDuplicates(nums2);
+    // Test Case 2: nums = [0,1,2,2,3,0,4,2], val = 2 -> k = 5
+    vector<int> nums2 = {0, 1, 2, 2, 3, 0, 4, 2};
+    int k2 = sol.removeElement(nums2, 2);
     cout << "Test Case 2 (k = " << k2 << "): ";
     for (int i = 0; i < k2; i++) cout << nums2[i] << " ";
     cout << endl;
