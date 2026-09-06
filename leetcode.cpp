5bf0f0e2f617ab7372ef161c545093755becc91d
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-09-06 12:03:11 +0530

    Add solution for LeetCode 28: Find the Index of the First Occurrence in a String

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,40 +1,36 @@
 #include <iostream>
-#include <vector>
+#include <string>
 
 using namespace std;
 
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
-        int k = 0;
+    int strStr(string haystack, string needle) {
+        int h = haystack.length();
+        int n = needle.length();
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != val) {
-                nums[k] = nums[i];
-                k++;
+        if (n > h) return -1;
+
+        for (int i = 0; i <= h - n; i++) {
+            if (haystack.substr(i, n) == needle) {
+                return i;
             }
         }
 
-        return k;
+        return -1;
     }
 };
 
 int main() {
     Solution sol;
 
-    // Test Case 1: nums = [3,2,2,3], val = 3 -> k = 2, nums = [2,2]
-    vector<int> nums1 = {3, 2, 2, 3};
-    int k1 = sol.removeElement(nums1, 3);
-    cout << "Test Case 1 (k = " << k1 << "): ";
-    for (int i = 0; i < k1; i++) cout << nums1[i] << " ";
-    cout << endl;
+    // Test Case 1: haystack = "sadbutsad", needle = "sad" -> 0
+    string h1 = "sadbutsad", n1 = "sad";
+    cout << "Test Case 1 Output: " << sol.strStr(h1, n1) << " (Expected: 0)" << endl;
 
-    // Test Case 2: nums = [0,1,2,2,3,0,4,2], val = 2 -> k = 5
-    vector<int> nums2 = {0, 1, 2, 2, 3, 0, 4, 2};
-    int k2 = sol.removeElement(nums2, 2);
-    cout << "Test Case 2 (k = " << k2 << "): ";
-    for (int i = 0; i < k2; i++) cout << nums2[i] << " ";
-    cout << endl;
+    // Test Case 2: haystack = "leetcode", needle = "leeto" -> -1
+    string h2 = "leetcode", n2 = "leeto";
+    cout << "Test Case 2 Output: " << sol.strStr(h2, n2) << " (Expected: -1)" << endl;
 
     return 0;
 }
