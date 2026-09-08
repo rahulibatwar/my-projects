f6c02a96c81beca9b50b5ef82b389c39675b8605
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-09-08 22:22:02 +0530

    Add LeetCode 35: Search Insert Position using Binary Search

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,53 +1,38 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <cmath>
 
 using namespace std;
 
 class Solution {
 public:
-    int threeSumClosest(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        int closest_sum = nums[0] + nums[1] + nums[2];
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
 
-        for (int i = 0; i < nums.size() - 2; i++) {
-            int left = i + 1;
-            int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-            while (left < right) {
-                int current_sum = nums[i] + nums[left] + nums[right];
-
-                if (current_sum == target) {
-                    return current_sum;
-                }
-
-                if (abs(current_sum - target) < abs(closest_sum - target)) {
-                    closest_sum = current_sum;
-                }
-
-                if (current_sum < target) {
-                    left++;
-                } else {
-                    right--;
-                }
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
 
-        return closest_sum;
+        return left;
     }
 };
 
 int main() {
     Solution sol;
 
-    // Example 1: [-1, 2, 1, -4], target = 1 -> Output: 2
-    vector<int> nums1 = {-1, 2, 1, -4};
-    cout << "Output 1: " << sol.threeSumClosest(nums1, 1) << " (Expected: 2)" << endl;
+    vector<int> nums = {1, 3, 5, 6};
 
-    // Example 2: [0, 0, 0], target = 1 -> Output: 0
-    vector<int> nums2 = {0, 0, 0};
-    cout << "Output 2: " << sol.threeSumClosest(nums2, 1) << " (Expected: 0)" << endl;
+    cout << "Target 5 Index: " << sol.searchInsert(nums, 5) << " (Expected: 2)" << endl;
+    cout << "Target 2 Index: " << sol.searchInsert(nums, 2) << " (Expected: 1)" << endl;
+    cout << "Target 7 Index: " << sol.searchInsert(nums, 7) << " (Expected: 4)" << endl;
 
     return 0;
 }
