28dbf70d99e74eb752bc72dc2e8b94890ea201ef
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-09-09 22:30:24 +0530

    Add solution for LeetCode 81: Search in Rotated Sorted Array II using Binary Search

--- leetcode.cpp
+++ leetcode.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) {
+    bool search(vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size() - 1;
 
@@ -13,26 +13,40 @@ public:
             int mid = left + (right - left) / 2;
 
             if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+                return true;
+            }
+
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+            }
+            else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
 
-        return left;
+        return false;
     }
 };
 
 int main() {
     Solution sol;
 
-    vector<int> nums = {1, 3, 5, 6};
-
-    cout << "Target 5 Index: " << sol.searchInsert(nums, 5) << " (Expected: 2)" << endl;
-    cout << "Target 2 Index: " << sol.searchInsert(nums, 2) << " (Expected: 1)" << endl;
-    cout << "Target 7 Index: " << sol.searchInsert(nums, 7) << " (Expected: 4)" << endl;
+    vector<int> nums1 = {2, 5, 6, 0, 0, 1, 2};
+    cout << boolalpha; // 1/0 की जगह true/false प्रिंट करने के लिए
+    cout << "Search 0: " << sol.search(nums1, 0) << " (Expected: true)" << endl;
+    cout << "Search 3: " << sol.search(nums1, 3) << " (Expected: false)" << endl;
 
     return 0;
 }
