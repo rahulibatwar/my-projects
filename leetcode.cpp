c113d9991d0aca026a3c354c28d1f48a1287bac3
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-09-14 19:32:55 +0530

    Add LeetCode 29: Divide Two Integers using Bit Manipulation

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,37 +1,45 @@
 #include <iostream>
-#include <vector>
+#include <climits>
+#include <cmath>
 
 using namespace std;
 
 class Solution {
 public:
-    char nextGreatestLetter(vector<char>& letters, char target) {
-        int left = 0;
-        int right = letters.size() - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (letters[mid] <= target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1) {
+            return INT_MAX;
         }
 
-        return letters[left % letters.size()];
+        bool isNegative = (dividend < 0) ^ (divisor < 0);
+
+        long long dvd = abs((long long)dividend);
+        long long dvs = abs((long long)divisor);
+
+        long long ans = 0;
+
+        while (dvd >= dvs) {
+            long long temp = dvs;
+            long long multiple = 1;
+
+            while (dvd >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+
+            dvd -= temp;
+            ans += multiple;
+        }
+
+        return isNegative ? -ans : ans;
     }
 };
 
 int main() {
     Solution sol;
 
-    vector<char> letters1 = {'c', 'f', 'j'};
-    cout << "Target 'a': " << sol.nextGreatestLetter(letters1, 'a') << " (Expected: c)" << endl;
-    cout << "Target 'c': " << sol.nextGreatestLetter(letters1, 'c') << " (Expected: f)" << endl;
-
-    vector<char> letters2 = {'x', 'x', 'y', 'y'};
-    cout << "Target 'z': " << sol.nextGreatestLetter(letters2, 'z') << " (Expected: x)" << endl;
+    cout << "10 / 3: " << sol.divide(10, 3) << " (Expected: 3)" << endl;
+    cout << "7 / -3: " << sol.divide(7, -3) << " (Expected: -2)" << endl;
 
     return 0;
 }
