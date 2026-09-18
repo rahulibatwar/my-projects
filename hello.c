8f578a68298a0589fb8f9008b606d7cfabaae592
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-09-18 11:37:01 +0530

    collage day 3 updated kay search kay  array for loopwarking methed

--- hello.c
+++ hello.c
@@ -2,16 +2,20 @@
 #include <string.h>
 int main(){
     int  a[5] = {10,20,30,40,50};
-    int i, kay;
+    int i, kay, flag = 0;
     printf("Enter the key to search: ");
     scanf("%d", &kay);
     for(int i=0; i<5; i=i+1){
         {
             if(a[i] == kay){
-                printf("Key found at index: %d\n", i);
+                printf("Key found at location: a[%d]", i);
+                flag = 1;
                 break;
             }
         }
+    }
+    if(flag == 0){
+        printf("Key not found");
     }
     return 0;
 }
