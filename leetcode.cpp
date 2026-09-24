6674ff0e2ceb750a9eb8ba7e4a97f612a68306a8
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-09-24 22:03:30 +0530

    Add LeetCode 918: Maximum Sum Circular Subarray using modified Kadane

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,41 +1,45 @@
 #include <iostream>
-#include <string>
-#include <stack>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
-    int longestValidParentheses(string s) {
-        stack<int> st;
-        st.push(-1);
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int totalSum = 0;
+        int currMax = 0, maxSum = nums[0];
+        int currMin = 0, minSum = nums[0];
 
-        int maxLen = 0;
+        for (int x : nums) {
+            currMax = max(x, currMax + x);
+            maxSum = max(maxSum, currMax);
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == '(') {
-                st.push(i);
-            } else {
-                st.pop();
-                if (st.empty()) {
-                    st.push(i);
-                } else {
-                    maxLen = max(maxLen, i - st.top());
-                }
-            }
+            currMin = min(x, currMin + x);
+            minSum = min(minSum, currMin);
+
+            totalSum += x;
         }
 
-        return maxLen;
+        if (maxSum < 0) {
+            return maxSum;
+        }
+
+        return max(maxSum, totalSum - minSum);
     }
 };
 
 int main() {
     Solution sol;
 
-    cout << "(() : " << sol.longestValidParentheses("(()") << " (Expected: 2)" << endl;
-    cout << ")()()) : " << sol.longestValidParentheses(")()())") << " (Expected: 4)" << endl;
-    cout << "Empty : " << sol.longestValidParentheses("") << " (Expected: 0)" << endl;
+    vector<int> nums1 = {1, -2, 3, -2};
+    cout << "Example 1: " << sol.maxSubarraySumCircular(nums1) << " (Expected: 3)" << endl;
+
+    vector<int> nums2 = {5, -3, 5};
+    cout << "Example 2: " << sol.maxSubarraySumCircular(nums2) << " (Expected: 10)" << endl;
+
+    vector<int> nums3 = {-3, -2, -3};
+    cout << "Example 3: " << sol.maxSubarraySumCircular(nums3) << " (Expected: -2)" << endl;
 
     return 0;
 }
