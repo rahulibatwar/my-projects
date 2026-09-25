07266380fdbbb1bfbcb832a1d70c4867ad224a02
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-09-25 22:03:27 +0530

    Add LeetCode 292: Nim Game using Game Theory math logic

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,45 +1,22 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
-    int maxSubarraySumCircular(vector<int>& nums) {
-        int totalSum = 0;
-        int currMax = 0, maxSum = nums[0];
-        int currMin = 0, minSum = nums[0];
-
-        for (int x : nums) {
-            currMax = max(x, currMax + x);
-            maxSum = max(maxSum, currMax);
-
-            currMin = min(x, currMin + x);
-            minSum = min(minSum, currMin);
-
-            totalSum += x;
-        }
-
-        if (maxSum < 0) {
-            return maxSum;
-        }
-
-        return max(maxSum, totalSum - minSum);
+    bool canWinNim(int n) {
+        return n % 4 != 0;
     }
 };
 
 int main() {
     Solution sol;
 
-    vector<int> nums1 = {1, -2, 3, -2};
-    cout << "Example 1: " << sol.maxSubarraySumCircular(nums1) << " (Expected: 3)" << endl;
-
-    vector<int> nums2 = {5, -3, 5};
-    cout << "Example 2: " << sol.maxSubarraySumCircular(nums2) << " (Expected: 10)" << endl;
-
-    vector<int> nums3 = {-3, -2, -3};
-    cout << "Example 3: " << sol.maxSubarraySumCircular(nums3) << " (Expected: -2)" << endl;
+    cout << boolalpha;
+    cout << "n = 4: " << sol.canWinNim(4) << " (Expected: false)" << endl;
+    cout << "n = 1: " << sol.canWinNim(1) << " (Expected: true)" << endl;
+    cout << "n = 2: " << sol.canWinNim(2) << " (Expected: true)" << endl;
+    cout << "n = 8: " << sol.canWinNim(8) << " (Expected: false)" << endl;
 
     return 0;
 }
