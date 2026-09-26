a8b0e01deff42848b77058e0c1f45f1717f6ce62
Rahul Ibatwar <rahulibatwar21@gmail.com>
2026-09-26 23:26:50 +0530

    Add LeetCode 703: Kth Largest Element in a Stream using Min-Heap

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,22 +1,43 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-class Solution {
+class KthLargest {
+private:
+    int K;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+
 public:
-    bool canWinNim(int n) {
-        return n % 4 != 0;
+    KthLargest(int k, vector<int>& nums) {
+        K = k;
+        for (int num : nums) {
+            add(num);
+        }
+    }
+    
+    int add(int val) {
+        minHeap.push(val);
+
+        if (minHeap.size() > K) {
+            minHeap.pop();
+        }
+
+        return minHeap.top();
     }
 };
 
 int main() {
-    Solution sol;
+    vector<int> nums = {4, 5, 8, 2};
+    KthLargest* kthLargest = new KthLargest(3, nums);
 
-    cout << boolalpha;
-    cout << "n = 4: " << sol.canWinNim(4) << " (Expected: false)" << endl;
-    cout << "n = 1: " << sol.canWinNim(1) << " (Expected: true)" << endl;
-    cout << "n = 2: " << sol.canWinNim(2) << " (Expected: true)" << endl;
-    cout << "n = 8: " << sol.canWinNim(8) << " (Expected: false)" << endl;
+    cout << "add(3): " << kthLargest->add(3) << " (Expected: 4)" << endl;
+    cout << "add(5): " << kthLargest->add(5) << " (Expected: 5)" << endl;
+    cout << "add(10): " << kthLargest->add(10) << " (Expected: 5)" << endl;
+    cout << "add(9): " << kthLargest->add(9) << " (Expected: 8)" << endl;
+    cout << "add(4): " << kthLargest->add(4) << " (Expected: 8)" << endl;
 
+    delete kthLargest;
     return 0;
 }
